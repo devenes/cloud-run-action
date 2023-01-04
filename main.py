02f2018fc8e1d7a270f72ee7a742ece453988b1d
Enes <ahmedenesturan@gmail.com>
2023-01-04 17:37:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World! I am running on Google Cloud Run"
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
