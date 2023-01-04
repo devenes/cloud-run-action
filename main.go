5a186f57871adec3d02616cc7d1b717425549f24
Enes <ahmedenesturan@gmail.com>
2023-01-04 20:00:33 +0300

    Update go

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World! This app is running on Cloud Run!\n")
+		tmpl, err := template.ParseFiles("template.html")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, nil)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
-	fmt.Printf("Server running on port 8080.\nYay! You're all set. Now, go to http://localhost:8080/ in your browser\nand you should see the message 'Hello, World!\n")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
