9f360b86c5971253d553c31662f883e7eed1346c
Enes <ahmedenesturan@gmail.com>
2023-01-19 19:44:13 +0300

    feat: add comments

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module hellogohttp/m/v2
+module devenes
 
 go 1.19
--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+	"fmt"           // package for input and output operations
+	"html/template" // package for template parsing
+	"net/http"      // package for HTTP server
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// parse the template file
 		tmpl, err := template.ParseFiles("template.html")
 
 		if err != nil {
+			// if there is an error, send an error message to the client
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// execute the template and write the result to the response writer
 		err = tmpl.Execute(w, nil)
 
 		if err != nil {
+			// if there is an error, send an error message to the client
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Listening on http://localhost:8080") // print a message to the console
+	http.ListenAndServe(":8080", nil)                 // start the server and listen on port 8080
 }
